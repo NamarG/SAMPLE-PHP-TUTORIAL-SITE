e7e5af1291c6c4076537050c7fe026b748cb7df0
NamarG <namar.ghani786@gmail.com>
2020-02-16 21:56:34 +0000

    php lesson 1

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
-echo "hello world"; 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test Site - Index</title>
+</head>
+
+<body bgcolor="ffcccb">    
+    <h1>hello World</h1>
+
+<?php 
+echo "hello ABZ from PHP!";
+echo "<br/>";
+echo "its namar";
 ?>
+
+
+
+<?php 
+$name = 'Namar'; 
+echo "<br/>";
+echo "<br/>";
+echo $name;
+echo "<h1>".$name." Is A Real G </h1>" 
+?>
+
+
+</body>
+</html>

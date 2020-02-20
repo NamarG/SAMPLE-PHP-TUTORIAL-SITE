0347e8197b87e537579fd697c5789cb1685998a5
NamarG <namar.ghani786@gmail.com>
2020-02-20 13:07:18 +0000

    loops and arrays complete

---
+++ stringmanip.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - String Manipulation</title>
+</head>
+<body>
+    <?php
+    $phrase1 = "'why didnt you do your homework'";
+    $phrase2 = "'my dog ate it'";
+
+
+    echo "<p>".$phrase1."</p><p>".$phrase2."</p>"
+    
+    ?>
+
+
+</body>
+</html>
---
+++ whiledowhileloop.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - do while/while loops</title>
+</head>
+<body>
+    
+    <H1>While Loop</H1>
+    <?PHP
+    $grade = 0;
+    while($grade<10){
+        echo "<p>grade is less than 10</p>";
+        $grade++;
+    }
+    ?>
+
+    <H1>Do While Loop</H1>
+    <?php
+    $grade = 0;
+        do{
+            echo "<p>i am do while loop</p>";
+            $grade++;
+        }while($grade<10);
+
+        $size = count($numbers);
+        echo "<h1>numbers in array = ".$size."</h1>";
+    ?>
+</body>
+</html>
